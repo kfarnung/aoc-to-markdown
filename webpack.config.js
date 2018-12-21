c429e3e371fce1db3362383dbad7ea026780d629
Kyle Farnung <kfarnung@outlook.com>
2018-12-21 09:12:01 -0800

    Upgrade packages and fix icons

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+    mode: "production",
     entry: {
         background_scripts: "./background_scripts/background.js",
         content_scripts: "./content_scripts/to-markdown.js"
