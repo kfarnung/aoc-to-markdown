250ca951a231d4c8e40d4e5d73f5a4dfd1aacc6f
Kyle Farnung <kfarnung@outlook.com>
2020-12-02 11:02:34 -0800

    Reduce the number of permissions required

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require("path");
 
 module.exports = {
-    mode: "production",
-    entry: {
-        background_scripts: "./background_scripts/background.js",
-        content_scripts: "./content_scripts/to-markdown.js"
-    },
-    output: {
-        path: path.resolve(__dirname, "addon"),
-        filename: "[name]/index.js"
-    }
+  mode: "production",
+  entry: {
+    background_scripts: ["./background_scripts/background.js"],
+    content_scripts: [
+      "./content_scripts/on-loaded.js",
+      "./content_scripts/to-markdown.js",
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "addon"),
+    filename: "[name]/index.js",
+  },
 };
